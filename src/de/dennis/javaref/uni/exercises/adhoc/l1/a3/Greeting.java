a6c2ac7761d200ab5298a12ccbafc4c0ef6b04f3
Dennis Dobirr <dennis.dobirr@realcore.de>
2026-01-30 22:49:12 +0100

    feat(uni/exercises/adhoc/li/a3): add adhoc exercise 3

---
+++ src/de/dennis/javaref/uni/exercises/adhoc/l1/a3/Greeting.java
@@ -0,0 +1,9 @@
+package de.dennis.javaref.uni.exercises.adhoc.l1.a3;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String firstName = args[0];
+        String lastName = args[1];
+        System.out.println("Hello " + firstName + " " + lastName);
+    }
+}
